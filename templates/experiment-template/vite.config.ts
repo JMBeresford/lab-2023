7657fb7636b3374bd9d8d36a2e463e0360081f52
John Beresford <jberesford@volcaus.com>
2023-05-13 19:00:36 -0700

    fix undefined destructuring

--- templates/experiment-template/vite.config.ts
+++ templates/experiment-template/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import { ExperimentData, type Experiment } from "experiment-data";
 import packageInfo from "./package.json";
 
-const { devPort } = ExperimentData[packageInfo.name as Experiment];
+const devPort = ExperimentData[packageInfo.name as Experiment]?.devPort;
 
 // https://vitejs.dev/config/
 export default defineConfig({
