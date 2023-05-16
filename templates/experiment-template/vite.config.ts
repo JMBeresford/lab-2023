9b511a199979f9ead9160a3a0bb227f94fe20a51
John Beresford <jberesford@volcaus.com>
2023-05-15 20:06:40 -0700

    refactor: use shared vite config

--- templates/experiment-template/vite.config.ts
+++ templates/experiment-template/vite.config.ts
@@ -1,57 +1,6 @@
-import { defineConfig } from "vite";
-import path from "path";
-import react from "@vitejs/plugin-react";
-import babel from "@rollup/plugin-babel";
-import resolve from "@rollup/plugin-node-resolve";
-import dts from "vite-plugin-dts";
-import commonjs from "@rollup/plugin-commonjs";
-import terser from "@rollup/plugin-terser";
-import typescript from "@rollup/plugin-typescript";
-import { glslify } from "vite-plugin-glslify";
+import config from "vite-custom-config/experiment";
+import { mergeConfig, defineConfig } from "vite";
 
-const extensions = [".js", ".jsx", ".ts", ".tsx"];
-const root = process.platform === "win32" ? path.resolve("/") : "/";
-const external = (id: string) => !id.startsWith(".") && !id.startsWith(root);
+const localConfig = defineConfig({});
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    glslify(),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
-  build: {
-    lib: {
-      entry: "src/index.ts",
-      formats: ["es"],
-      fileName: "index",
-    },
-    rollupOptions: {
-      input: "./src/index.ts",
-      output: {
-        dir: "dist",
-        format: "esm",
-        preserveModules: true,
-      },
-      external,
-      plugins: [
-        commonjs(),
-        // glslify(),
-        babel({
-          exclude: "node_modules/**",
-          extensions,
-          babelHelpers: "runtime",
-          presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
-          plugins: [
-            ["@babel/plugin-transform-runtime", { regenerator: false, useESModules: true }],
-          ],
-        }),
-        typescript({ compilerOptions: { module: "esnext" } }),
-        resolve({ extensions }),
-        terser(),
-      ],
-    },
-  },
-});
+export default mergeConfig(config, localConfig, true);
