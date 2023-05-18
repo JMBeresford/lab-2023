5dd64dc7e142ba044e447cd30d39400f4a1315bf
John Beresford <jberesford@volcaus.com>
2023-05-17 20:01:00 -0700

    experiment done

--- experiments/oceanic-horizon/src/Scene/Scene.tsx
+++ experiments/oceanic-horizon/src/Scene/Scene.tsx
@@ -1,9 +1,26 @@
 import { Canvas } from "@react-three/fiber";
+import Sky from "./Sky";
+import Ocean from "./Ocean";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
 export function Scene() {
   return (
     <Canvas>
-      <></>
+      <Sky />
+      <Ocean />
+
+      <PerspectiveCamera makeDefault={true} />
+
+      <OrbitControls
+        position={[0, 2.5, 0]}
+        enablePan={false}
+        enableDamping={true}
+        dampingFactor={0.025}
+        target={[0, 2.5, 0]}
+        enableZoom={false}
+        minPolarAngle={Math.PI / 6}
+        maxPolarAngle={Math.PI / 2}
+      />
     </Canvas>
   );
 }
