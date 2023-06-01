f31cf4b72189d77d5476d7032f666d8906c90b98
John Beresford <jberesford@volcaus.com>
2023-05-31 20:21:51 -0700

    fix: shot in the dark 2

--- apps/lab/app/experiments/ExtraDimensionalInputForm/page.tsx
+++ apps/lab/app/experiments/ExtraDimensionalInputForm/page.tsx
@@ -2,17 +2,13 @@
 
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
-const Experiment = dynamic(
-  () => import("extra-dimensional-input-form").then((mod) => mod.Experiment),
-  {
-    ssr: false,
-  },
-);
+// const Experiment = dynamic(
+//   () => import("extra-dimensional-input-form").then((mod) => mod.Experiment),
+//   {
+//     ssr: false,
+//   },
+// );
 
 export default function Page() {
-  return (
-    <Suspense fallback={null}>
-      <Experiment />
-    </Suspense>
-  );
+  return <Suspense fallback={null}>{/* <Experiment /> */}</Suspense>;
 }
