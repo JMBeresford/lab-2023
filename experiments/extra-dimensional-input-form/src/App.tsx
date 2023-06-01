ebd661982560af7c02104814a1966b1688af8362
John Beresford <jberesford@volcaus.com>
2023-05-31 21:07:00 -0700

    fix: add bg color to 3d input experiment

--- experiments/extra-dimensional-input-form/src/App.tsx
+++ experiments/extra-dimensional-input-form/src/App.tsx
@@ -2,7 +2,7 @@ import { Scene } from "./LoginScene";
 
 export function Experiment() {
   return (
-    <div style={{ height: "100%", width: "100%" }}>
+    <div style={{ height: "100%", width: "100%", backgroundColor: "white" }}>
       <Scene />
     </div>
   );
