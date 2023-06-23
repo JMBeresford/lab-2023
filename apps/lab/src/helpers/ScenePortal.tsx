b5f72cf0415cfaca8953e51792dcab12f748eda1
John Beresford <jberesford@volcaus.com>
2023-06-23 14:50:05 -0700

    [lab] conditionally render home scene

--- apps/lab/src/helpers/ScenePortal.tsx
+++ apps/lab/src/helpers/ScenePortal.tsx
@@ -2,8 +2,15 @@
 
 import { ReactNode, Suspense, forwardRef, useImperativeHandle, useRef } from "react";
 import { SceneTunnel } from "./tunnel";
+import { usePathname } from "next/navigation";
 
 export function ScenePortal(props: { children: ReactNode }) {
+  const path = usePathname();
+
+  if (!["/", "/experiments"].includes(path)) {
+    return null;
+  }
+
   return (
     <>
       <Suspense fallback={null}>
