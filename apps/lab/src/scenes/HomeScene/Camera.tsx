cde1f9d0cc3a72cc07e767e896238b515ab9f478
John Beresford <jberesford@volcaus.com>
2023-08-05 21:22:09 -0700

    lint fixes

--- apps/lab/src/scenes/HomeScene/Camera.tsx
+++ apps/lab/src/scenes/HomeScene/Camera.tsx
@@ -5,16 +5,16 @@ import { useEffect, useRef, useState } from "react";
 import { Euler, Group, PerspectiveCamera as PerspectiveCameraProps, Vector3 } from "three";
 import { damp } from "three/src/math/MathUtils";
 
-const Scenes = ["/", "/experiments"] as const;
+const Scene = ["/", "/experiments"] as const;
 
-type Scenes = typeof Scenes[number];
+type Scene = typeof Scene[number];
 
 type Rig = {
   position: Vector3 | { x: number; y: number; z: number };
   rotation: Euler | { x: number; y: number; z: number };
 };
 
-const Rigs: Record<Scenes, Rig> = {
+const Rigs: Record<Scene, Rig> = {
   "/": {
     position: { x: 0, y: 1.5, z: 0 },
     rotation: { x: 0, y: 0, z: 0 },
@@ -26,13 +26,14 @@ const Rigs: Record<Scenes, Rig> = {
 };
 
 export function Camera() {
-  const groupRef = useRef<Group>(undefined);
-  const ref = useRef<PerspectiveCameraProps>(undefined);
+  const groupRef = useRef<Group>(null);
+  const ref = useRef<PerspectiveCameraProps>(null);
   const path = usePathname();
   const size = useThree((state) => state.size);
-  const [rig, setRig] = useState<Rig>(undefined);
+  const [rig, setRig] = useState<Rig>();
 
   useEffect(() => {
+    if (!ref.current) return;
     if (size.width <= 768) {
       ref.current.fov = 75;
     } else {
@@ -41,8 +42,8 @@ export function Camera() {
   }, [size]);
 
   useEffect(() => {
-    const newRig = Rigs[path];
-    if (rig == undefined && newRig != undefined) {
+    const newRig = Rigs[path as Scene];
+    if (groupRef.current && rig == undefined && newRig != undefined) {
       groupRef.current.position.set(newRig.position.x, newRig.position.y, newRig.position.z);
       groupRef.current.rotation.set(newRig.rotation.x, newRig.rotation.y, newRig.rotation.z);
     }
@@ -50,7 +51,7 @@ export function Camera() {
   }, [path, rig]);
 
   useFrame(({ mouse }, dt) => {
-    if (!rig) return;
+    if (!rig || !ref.current) return;
 
     const x = mouse.x * 0.1;
     const y = mouse.y * 0.1;
@@ -59,7 +60,7 @@ export function Camera() {
     const rotation = rig.rotation;
     const lambda = 2;
 
-    if (position && rotation) {
+    if (position && rotation && groupRef.current) {
       groupRef.current.position.x = damp(groupRef.current.position.x, position.x, lambda, dt);
       groupRef.current.position.y = damp(groupRef.current.position.y, position.y, lambda, dt);
       groupRef.current.position.z = damp(groupRef.current.position.z, position.z, lambda, dt);
