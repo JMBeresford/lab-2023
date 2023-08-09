277b8dd9102c709fe1f8c05c4786b35382c03083
John Beresford <jberesford@volcaus.com>
2023-08-08 17:12:46 -0700

    test experiment change

--- experiments/retro-gaming/src/utils/colors.ts
+++ experiments/retro-gaming/src/utils/colors.ts
@@ -1,4 +1,4 @@
-export function hexToBrightness(hex: string) {
+export function hexToBrightness(hex: string): number {
   const r = parseInt(hex.slice(1, 3), 16) / 255;
   const g = parseInt(hex.slice(3, 5), 16) / 255;
   const b = parseInt(hex.slice(5, 7), 16) / 255;
